9d04ce13cc3d059818f9dafa9d5de966ed3879f1
Ruben <just14zy@gmail.com>
2017-12-21 19:54:41 +0300

    Подтягиваем данные

--- js/main.js
+++ js/main.js
@@ -1,26 +1,67 @@
 
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
+Vue.component('quest', {
+  props: ['quest'],
+  computed: {
+        countPainters() {
+          return 56 //this.quest.id//'#' + this.name;
+        }
+      },
+  template: `
+  <div class="card p-2 mb-3">
+    <div class="card-block">
+      <div class="row">
+        <div class="col-8"><h4>{{quest.title}}</h4></div>
+        <div class="col-4 text-right">{{quest.completed}}</div>
+      </div>
+      {{quest.description}}
+      <div class="row pt-2">
+        <div class="col-8">{{quest.difficult}}</div>
+        <div class="col-4 text-right">{{countPainters}}</div>
+      </div>
+    </div>
+  </div>
+  `
 });
+
+Vue.component('quests-list', {
+  template: `
+  <div>
+    <quest v-for="quest in quests" :quest="quest"></quest>
+  </div>
+  `,
+  data() {
+    return {
+      quests: [
+        {id: "monet", difficult: "easy", title: "Monet or Manet?", description: "Know the difference in 2 minutes", painters: [24,21,23,12,1,2], completed: false, hidden: false},
+        {id: "picasso", difficult: "easy", title: "Picasso or Dali?", description: "Know the difference in 2 minutes", painters: [24,21,23,12,1,2], completed: false, hidden: false},
+        {id: "popular", difficult: "basic", title: "Popular painters", description: "Know the difference in 2 minutes", painters: [24,21,23,12,1,2], completed: false, hidden: false},
+        {id: "french", difficult: "basic", title: "French painters", description: "Know the difference in 2 minutes", painters: [24,21,23,12,1,2], completed: false, hidden: false},
+        {id: "italian", difficult: "basic", title: "Italian painters", description: "Know the difference in 2 minutes", painters: [24,21,23,12,1,2], completed: false, hidden: false},
+        {id: "all", difficult: "hard", title: "All painters", description: "Know the difference in 2 minutes", painters: [24,21,23,12,1,2], completed: false, hidden: false},
+        //сложность, список художников, картинка, описание, название, id, пройден/не пройден, secret
+      ]
+    }
+  }
+});
+
 
 var app = new Vue({
     el: '#app',
-    data: {
-        message: "Hello World",
-        food_items: ["hamburger", "hotdog", "fries"],
-        seen: true,
-        groceryList: [
-            { text: 'Vegetables' },
-            { text: 'Cheese' },
-            { text: 'Whatever else humans are supposed to eat' }
-        ]
-    },
-    methods: {
-        buttonClick: function() {
-            this.seen = !this.seen;
-        }
-    }
+    // data: {
+    //     message: "Hello World",
+    //     food_items: ["hamburger", "hotdog", "fries"],
+    //     seen: true,
+    //     groceryList: [
+    //         { text: 'Vegetables' },
+    //         { text: 'Cheese' },
+    //         { text: 'Whatever else humans are supposed to eat' }
+    //     ]
+    // },
+    // methods: {
+    //     buttonClick: function() {
+    //         this.seen = !this.seen;
+    //     }
+    // }
 });
 
 

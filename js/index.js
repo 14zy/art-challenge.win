cc01a4076ec0c8bd67e836545cda59abe8489aa0
Ruben <just14zy@gmail.com>
2018-01-07 11:53:33 +0300

    Quest list

--- js/index.js
+++ js/index.js
@@ -4,17 +4,18 @@ Vue.component('quest', {
   <div v-if="!quest.hidden" :style="style" class="text-muted quest p-2 mb-3" @click="window.app.loading = true; setTimeout(function(){window.location.href='game.html?quest='+quest.id+'&difficult='+quest.difficult},500);">
 
         <div :class="{ 'text-success': quest.completed, 'text-primary': quest.available}">
-          <span class="text-right pr-1" style="font-size: 22px; float: right">
-
+          <span class="text-right pr-1 float-right" style="font-size: 20px; ">
             <i v-if="quest.available" class="fa fa-play-circle"></i>
             <i v-if="quest.completed" class="fa fa-certificate text-success"></i>
             <i v-if="!quest.completed && !quest.available" class="fa fa-lock"></i>
-
           </span>
           <h4>{{quest.title}} </h4>
           {{quest.description}}
-          <br>{{quest.painters}} Painters
-          {{quest.difficult}}
+          <br>
+          <span class="text-capitalize small">{{quest.difficult}}</span>
+          <span class="text-right pr-1 small float-right">
+            {{quest.painters}} Painters
+          </span>
         </div>
   </div>`,
   data: function () {

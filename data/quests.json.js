cc547ccb522777628b1de2dc18c814590e63e2ae
Ruben <just14zy@gmail.com>
2018-01-22 04:10:42 +0300

    Quest list

--- data/quests.json.js
+++ data/quests.json.js
@@ -3,7 +3,6 @@ var quests = [
     id: "picasso",
     difficult: "easy",
     title: "Dali or Picasso?",
-    description: "Great painters of the world",
     completed: false,
     available: false,
     painters: [28, 15]
@@ -11,7 +10,6 @@ var quests = [
     id: "monet",
     difficult: "easy",
     title: "Monet or Manet?",
-    description: "Know the difference in 3 minutes",
     completed: false,
     available: false,
     painters: [9, 17]
@@ -20,108 +18,96 @@ var quests = [
     id: "popularEasy",
     difficult: "easy",
     title: "Popular Painters",
-    description: "Popular painters of the world",
     completed: false,
     available: false,
     painters: [21, 15, 9, 28, 45, 54, 112, 63, 17, 30]
   },
   {
+    id: "surrealism",
+    difficult: "easy",
+    title: "Surrealism",
+    completed: false,
+    available: false,
+    painters: [33, 14, 15, 79, 82]
+  },
+  {
     id: "american",
-    difficult: "basic",
+    difficult: "easy",
     title: "American Painters",
-    description: "Let see the treasures",
     completed: false,
     available: false,
     painters: [83, 116, 118, 18, 60]
   },
   {
-    id: "french",
-    difficult: "basic",
-    title: "French Painters",
-    description: "Get know them",
-    completed: false,
-    available: false,
-    painters: [2, 9, 17, 30, 36, 40, 49, 53, 57, 58, 61, 64, 65, 69, 70, 73, 75, 77, 93, 94, 96, 97]
-  },
-  {
     id: "impressionism",
     difficult: "basic",
     title: "Impressionism",
-    description: "Know the difference in 2 minutes",
     completed: false,
     available: false,
     painters: [2, 3, 9, 16, 17, 21, 30, 36, 49, 53, 57, 60, 61, 69, 77, 84, 94, 96]
   },
+  // {
+  //   id: "abstractionism",
+  //   difficult: "basic",
+  //   title: "Abstractionism",
+  //   completed: false,
+  //   available: false,
+  //   painters: [1]
+  // },
+  // {
+  //   id: "artdeco",
+  //   difficult: "basic",
+  //   title: "Art Deco",
+  //   completed: false,
+  //   available: false,
+  //   painters: [56, 44]
+  // },
   {
-    id: "abstract",
+    id: "italian",
     difficult: "basic",
-    title: "Abstractionism",
-    description: "Know the difference in 2 minutes",
+    title: "Italian Painters",
     completed: false,
     available: false,
-    painters: [1]
-  },
-  {
-    id: "artdeco",
-    difficult: "basic",
-    title: "Art Deco",
-    description: "Know the difference in 2 minutes",
-    completed: false,
-    available: false,
-    painters: [1]
+    painters: [1, 41, 42, 43, 45, 55, 87, 91, 95, 101, 106, 108, 112]
   },
   {
     id: "avantgarde",
     difficult: "basic",
     title: "Avant-garde",
-    description: "Know the difference in 2 minutes",
     completed: false,
     available: false,
-    painters: [1]
+    painters: [26, 4, 40, 52, 1, 63, 83, 118, 28, 80, 82, 49, 94, 15]
   },
   {
-    id: "italian",
+    id: "french",
     difficult: "basic",
-    title: "Italian Painters",
-    description: "Know the difference in 2 minutes",
+    title: "French Painters",
     completed: false,
     available: false,
-    painters: [1]
-  },
-  {
-    id: "surrealism",
-    difficult: "basic",
-    title: "Surrealism",
-    description: "Know the difference in 2 minutes",
-    completed: false,
-    available: false,
-    painters: [1]
-  },
-  {
-    id: "popular",
-    difficult: "basic",
-    title: "All Popular Painters",
-    description: "Popular painters of the world",
-    completed: false,
-    available: false,
-    painters: [1, 4, 7, 9, 14, 15, 17, 19, 21, 22, 24, 26, 27, 28, 29, 30, 32, 33, 34, 35, 36, 39, 40, 41, 42, 43, 45, 46, 49, 50, 53, 54, 55, 57, 58, 61, 62, 63, 69, 73, 75, 77, 79, 80, 82, 83, 94, 95, 112, 118]
-  },
-  {
-    id: "russian",
-    difficult: "hard",
-    title: "Russian Painters",
-    description: "Let see the treasures",
-    completed: false,
-    available: false,
-    painters: [3, 4, 5, 6, 8, 10, 11, 12, 13, 16, 19, 20, 23, 25, 26, 27, 37, 38, 44, 47, 48, 76, 81, 84, 85, 86, 103, 105, 113]
+    painters: [2, 9, 17, 30, 36, 40, 49, 53, 57, 58, 61, 64, 65, 69, 70, 73, 75, 77, 93, 94, 96, 97]
   },
   {
     id: "northernRenaissance",
     difficult: "hard",
     title: "Northern Renaissance",
-    description: "Renaissance Era painters",
     completed: false,
     available: false,
     painters: [24, 35, 39, 41, 42, 45, 50, 55, 87, 89, 90, 91, 92, 95, 98, 100, 101, 104, 106, 108, 110, 111, 112, 114]
-  }
+  },
+  {
+    id: "russian",
+    difficult: "hard",
+    title: "Russian Painters",
+    completed: false,
+    available: false,
+    painters: [3, 4, 5, 6, 8, 10, 11, 12, 13, 16, 19, 20, 23, 25, 26, 27, 37, 38, 44, 47, 48, 76, 81, 84, 85, 86, 103, 105, 113]
+  },
+  {
+    id: "popular",
+    difficult: "hard",
+    title: "All Famous Painters",
+    completed: false,
+    available: false,
+    painters: [1, 4, 7, 9, 14, 15, 17, 19, 21, 22, 24, 26, 27, 28, 29, 30, 32, 33, 34, 35, 36, 39, 40, 41, 42, 43, 45, 46, 49, 50, 53, 54, 55, 57, 58, 61, 62, 63, 69, 73, 75, 77, 79, 80, 82, 83, 94, 95, 112, 118]
+  },
 ]
